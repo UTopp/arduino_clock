8c3836c174d9e896d6b8355b821eeff6a52c07ac
UTopp <umberto.toppino@hotmail.com>
2016-02-21 16:03:09 +0100

    Important fixes to the code managing the time
    Solved the bug that didn't allow the time in millisecond to be correctly
    converted in hours, minutes and seconds.
    Also minor fixes for readability and to eliminate useless parts of the
    code

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -2,41 +2,23 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-unsigned long tot_time, hour, minute, second;
-
+unsigned long tot_time_sec, hour, minute, second;
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
   lcd.print("Current Time :");
-  lcd.setCursor(14,0);
-
-  pinMode(8, INPUT);        // pins used for manual settings
-  pinMode(9, INPUT);        // ***
-  
-  hour = 0;
-  minute = 0;
-  second = 0;
-  tot_time = 0;
 }
 
 void loop() {
-  unsigned long hour_set = 1 * 60 * 60 * 1000;
-  unsigned long minute_set = 58 * 60 * 1000;
-
-
   // put your main code here, to run repeatedly:
-  if(digitalRead(8) == HIGH)                                  //used to change the time without needing to access the code later on
-    hour_set++;
-  if(digitalRead(9) == HIGH)                                  //same as above
-    minute_set++; 
-  tot_time = hour_set + minute_set + millis();                //actual time considering manual settings
-  hour =  tot_time/(60 * 60 * 1000);                                              // *** THIS PART OF THE CODE SEEMS NOT TO WORK ***
-  minute = (tot_time - (hour*(60 * 60 * 1000)))/(60 * 1000);                      // *** THIS PART OF THE CODE SEEMS NOT TO WORK ***
-  second = (tot_time - (hour*(60 * 60 * 1000)) - (minute*(60 * 1000)))/1000;      // *** THIS PART OF THE CODE SEEMS NOT TO WORK ***
+  tot_time_sec = millis()/1000;                         //time in seconds
+  hour =  tot_time_sec / (60*60);
+  minute = (tot_time_sec % (60*60)) / (60);
+  second = (tot_time_sec % (60*60)) % (60);
 
-  lcd.setCursor(0, 1);                //fixes overlapping digits
-    lcd.print(second);
+  lcd.setCursor(0, 1);                                  //fixes overlapping digits
+    lcd.print(hour);
   if(hour>9){
     lcd.print(hour);
   } else {
@@ -67,5 +49,4 @@ void loop() {
     lcd.print(second);
   }                                 // end of overlapping digits fix
   
-  delay(250);                       // intendet to limit amount of data sent to the display
 }
